9b37aed9ce2952298b029ea115362f82c4b27bd2
dododo25 <dmytro.terekhov@outlook.com>
2024-08-01 10:48:14 +0300

    Aplly initial angle differential

--- src/celestial/planet/Planet.js
+++ src/celestial/planet/Planet.js
@@ -18,31 +18,43 @@ class Planet extends React.Component {
         <g className='orbit'>
           <circle cx='0' cy='0' r={orbitRadius} stroke='#FAFAFA' strokeWidth='0.5' strokeDasharray='12' fill='transparent' />
         </g>
-        <g>
-          <g transform={`translate(${orbitRadius}, 0)`}>
-            <g className='moons'>
-              {this.props.children}
-              <animateTransform
-                attributeName='transform'
-                attributeType='XML'
-                type='rotate'
-                from='0'
-                to={360 * oppositeMultiplier}
-                dur={`${period}s`}
-                repeatCount='indefinite' />
+        <g transform={`rotate(${this.props.initialAngle * -1})`}>
+          <g>
+            <g transform={`translate(${orbitRadius}, 0)`}>
+              <g className='moons'>
+                {this.props.children}
+                <animateTransform
+                  attributeName='transform'
+                  attributeType='XML'
+                  type='rotate'
+                  from='0'
+                  to={360 * oppositeMultiplier}
+                  dur={`${period}s`}
+                  repeatCount='indefinite' />
+              </g>
+              <g transform={`rotate(${this.props.initialAngle * 1})`}>
+                <g>
+                  {this.view}
+                  <animateTransform
+                    attributeName='transform'
+                    attributeType='XML'
+                    type='rotate'
+                    from='0'
+                    to={360 * oppositeMultiplier}
+                    dur={`${period}s`}
+                    repeatCount='indefinite' />
+                </g>
+              </g>
             </g>
-            <g>
-              {this.view}
-            </g>
+            <animateTransform
+              attributeName='transform'
+              attributeType='XML'
+              type='rotate'
+              from='0'
+              to={-360 * oppositeMultiplier}
+              dur={`${period}s`}
+              repeatCount='indefinite' />
           </g>
-          <animateTransform
-            attributeName='transform'
-            attributeType='XML'
-            type='rotate'
-            from='0'
-            to={-360 * oppositeMultiplier}
-            dur={`${period}s`}
-            repeatCount='indefinite' />
         </g>
       </g>
     );
